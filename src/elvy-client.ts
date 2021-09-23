7509654ab8ad99446620d91898048af69ae052f7
iRandell <irandellhart@gmail.com>
2021-09-23 15:37:21 +0300

    refactor: use method signature for functions

--- src/elvy-client.ts
+++ src/elvy-client.ts
@@ -7,48 +7,48 @@ import type { RequestResult } from './request-result'
 import type { RequestSetup } from './request-setup'
 
 export interface ElvyClient {
-  on: <TEventName extends keyof EventHandlerMap>(
+  on<TEventName extends keyof EventHandlerMap>(
     eventName: TEventName,
     handler: EventHandlerMap[TEventName]
-  ) => EventHandlerMap[TEventName]
+  ): EventHandlerMap[TEventName]
 
-  off: <TEventName extends keyof EventHandlerMap>(
+  off<TEventName extends keyof EventHandlerMap>(
     eventName: TEventName,
     handler: EventHandlerMap[TEventName]
-  ) => void
+  ): void
 
-  get: <TResult = unknown>(
+  get<TResult = unknown>(
     url: string,
     options?: RequestOptions
-  ) => Promise<RequestResult<TResult>>
+  ): Promise<RequestResult<TResult>>
 
-  head: (url: string, options?: RequestOptions) => Promise<RequestResult>
+  head(url: string, options?: RequestOptions): Promise<RequestResult>
 
-  post: <TPayload = unknown, TResult = unknown>(
-    url: string,
-    payload?: TPayload,
-    options?: RequestOptions
-  ) => Promise<RequestResult<TResult>>
-
-  put: <TPayload = unknown, TResult = unknown>(
+  post<TPayload = unknown, TResult = unknown>(
     url: string,
     payload?: TPayload,
     options?: RequestOptions
-  ) => Promise<RequestResult<TResult>>
+  ): Promise<RequestResult<TResult>>
 
-  delete: <TPayload = unknown, TResult = unknown>(
+  put<TPayload = unknown, TResult = unknown>(
     url: string,
     payload?: TPayload,
     options?: RequestOptions
-  ) => Promise<RequestResult<TResult>>
+  ): Promise<RequestResult<TResult>>
 
-  patch: <TPayload = unknown, TResult = unknown>(
+  delete<TPayload = unknown, TResult = unknown>(
     url: string,
     payload?: TPayload,
     options?: RequestOptions
-  ) => Promise<RequestResult<TResult>>
+  ): Promise<RequestResult<TResult>>
 
-  request: <TPayload = unknown, TResult = unknown>(
+  patch<TPayload = unknown, TResult = unknown>(
+    url: string,
+    payload?: TPayload,
+    options?: RequestOptions
+  ): Promise<RequestResult<TResult>>
+
+  request<TPayload = unknown, TResult = unknown>(
     setup: RequestSetup<TPayload>
-  ) => Promise<RequestResult<TResult>>
+  ): Promise<RequestResult<TResult>>
 }
